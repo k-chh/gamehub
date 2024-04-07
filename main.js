c49cc0129f16fa08d09bb5c0455657b90f1bb1c5
Kayla Claire Habon Huynh <144626344+k-chh@users.noreply.github.com>
2024-04-07 22:40:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+/* TOP NAVIGATION */
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+/* SLIDESHOW */
+ 
+ 
+/** multiple slide */
+var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"]
+showDivs(1, 0);
+showDivs(1, 1);
+
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
